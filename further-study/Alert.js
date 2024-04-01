494ef7acb7f4f5f430d7c5e7c77e190a67272ac4
Yagiz Ulasoglu <yulasoglu@gmail.com>
2024-04-01 12:28:53 -0700

    Further Study done

---
+++ further-study/Alert.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function Alert({message}) {
+  return (
+  <div className="alert">
+    <p>{message}</p>
+  </div>
+  );
+}
---
+++ further-study/App.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function App() {
+  return (
+    <div>
+      <Alert message="This is an alert!" />
+    </div>
+  )
+}
