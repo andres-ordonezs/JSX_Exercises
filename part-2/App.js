a274cdc48d62db779a4676d2909367209e8e2339
Yagiz Ulasoglu <yulasoglu@gmail.com>
2024-04-01 12:02:30 -0700

    Part 2 is done

---
+++ part-2/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <Tweet username="IslandBoi" name="Kadeem" date="04/01/2024" message="React" />
+      <Tweet username="NotIslandBoi" name="Edmond" date="03/01/2024" message="Node" />
+      <Tweet username="Hey" name="Kate" date="02/01/2024" message="Express" />
+    </div>
+  )
+}
